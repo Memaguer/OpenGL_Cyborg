bef07281971de9bedd11b519cfb48b41091c536e
Guillermo Barrientos <gbarrientos@alabool.mx>
2018-02-05 12:14:52 -0600

    Initial Commit

---
+++ Cyborg/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Cyborg
+//
+//  Created by MBG on 2/5/18.
+//  Copyright © 2018 MBG. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
